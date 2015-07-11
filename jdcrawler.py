9f7bd58b51b94fb3a774bd77dff636e6525e91eb
stevenlordiam <stevenlordiam@gmail.com>
2015-07-11 18:00:32 -0400

    update jdcrawler.py with more user agent list

--- jdcrawler.py
+++ jdcrawler.py
@@ -1,18 +1,24 @@
-import urllib2
-import urllib
-import os
-import sys
+import urllib2, urllib, os, sys, random
 from bs4 import BeautifulSoup
 from progress.bar import Bar
 start_page = int(sys.argv[1])
 end_page = int(sys.argv[2]) + 1
 
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-       'Accept-Encoding': 'none',
-       'Accept-Language': 'en-US,en;q=0.8',
-       'Connection': 'keep-alive'}
+user_agent_list = [
+                    'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',
+                    'Opera/9.25 (Windows NT 5.1; U; en)',
+                    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+                    'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)',
+                    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
+                    'Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9',
+                    "Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Ubuntu/11.04 Chromium/16.0.912.77 Chrome/16.0.912.77 Safari/535.7",
+                    "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0 ",
+
+                    ] 
+       
+agent = random.choice(user_agent_list)
+
+headers = {'User-Agent': agent, 'Connection': 'keep-alive'}
 
 def fetch_data(url):
     try:
